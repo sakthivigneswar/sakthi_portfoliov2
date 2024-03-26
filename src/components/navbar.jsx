7adba010b4136e3a31f2f05bcfcf70ace52c02a3
Butcher <largesizepizaa@gmail.com>
2024-03-26 11:54:25 +0530

    Fixed send message button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
         >
-          <span className="text-white mr-1">Sakthi</span>
+          <span className="text-white mr-1">Test</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
             VR
           </span>
