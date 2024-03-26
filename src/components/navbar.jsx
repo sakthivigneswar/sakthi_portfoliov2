8779e3ec9cfbe8910bc5dd3da35d08a145ff5374
Butcher <largesizepizaa@gmail.com>
2024-03-26 12:09:59 +0530

    fixed target links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
         >
-          <span className="text-white mr-1">Test</span>
+          <span className="text-white mr-1">Sakthi</span>
           <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
             VR
           </span>
@@ -90,16 +90,16 @@ const Navbar = () => {
       </div>
       {/* SOCIAL */}
       <div className="hidden md:flex gap-4 w-1/3">
-        <Link href="google.com">
+        <Link href="https://github.com/sakthivigneswar" target="_blank" >
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="/">
+        <Link href="https://www.instagram.com/sakthivr_46/" target="_blank" >
           <Image src="/instagram.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="/">
+        <Link href="https://www.facebook.com/people/Jessie-Pinkman/pfbid02NjcWgosb2xgjqaroHGdivpoScVvnXEyUqm39d4fR5HzYhb5yBeZ3RWL7tXW6LryBl/?mibextid=ZbWKwL" target="_blank">
           <Image src="/facebook.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="/">
+        <Link href="https://www.linkedin.com/in/sakthi-vigneswar-201b02292/" target="_blank">
           <Image src="/linkedin.png" alt="" width={24} height={24} />
         </Link>
       </div>
